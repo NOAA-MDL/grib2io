dd0527b2fac2dfbdc2b2d40052151779d107616f
Eric Engle <eengl@users.noreply.github.com>
2020-11-30 14:11:47 -0500

    Updating docs for tables
    - Updating GRIB2 table documentation.

--- grib2io/tables/__init__.py
+++ grib2io/tables/__init__.py
@@ -3,11 +3,29 @@ from .section1 import *
 from .section3 import *
 from .section4 import *
 from .section5 import *
-
 from .originating_centers import *
 
 
 def get_table(table,expand=False):
+    """
+    Return GRIB2 code table as a dictionary.
+
+    Parameters
+    ----------
+
+    **`table : str`**
+
+    Code table number.  Example: '1.0'
+
+    **`expand : bool`**
+
+    When **`True`**, expand output dictionary where keys are a range. Default is **`false`**
+
+    Returns
+    -------
+
+    **`dict`**    
+    """
     tbl = globals()['table_'+table.replace('.','_')]
     if expand:
         _tbl = {}
@@ -24,6 +42,23 @@ def get_table(table,expand=False):
 
 def get_value_from_table(value,table):
     """
+    Return the meaning/definition given a GRIB2 code table `value` and `table`.
+
+    Parameters
+    ----------
+
+    **`value : str`**
+
+    Code table value.  Function will convert integer to string.  Example: '5'
+
+    **`table : str`**
+
+    Code table number.  Example: '1.0'
+
+    Returns
+    -------
+
+    **`str` or `list`**
     """
     try:
         tbl = get_table(table,expand=True)
@@ -35,6 +70,27 @@ def get_value_from_table(value,table):
 
 def get_varname_from_table(discipline,parmcat,parmnum):
     """
+    Return the GRIB2 variable name information given values of `discipline`,
+    `parmcat`, and `parmnum`. NOTE: This functions allows for all arguments
+    to be converted to a string type if arguments are integer.
+
+    Parameters
+    ----------
+
+    **`discipline : str`**
+
+    Discipline code value of a GRIB2 message.
+
+    **`parmcat : str`**
+
+    Parameter Category value of a GRIB2 message.
+
+    **`parmnum : str`**
+
+    Parameter Number value of a GRIB2 message.
+
+    Returns
+    -------
     """
     if isinstance(discipline,int): discipline = str(discipline)
     if isinstance(parmcat,int): parmcat = str(parmcat)
