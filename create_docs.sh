f21c69e987ea7997b74e9c513424959aa92ab0af
Eric Engle <eengl@users.noreply.github.com>
2021-01-28 13:12:02 -0500

    Update create_docs.sh
    Updated to use pdoc instead of pdoc3 (they are different!)

--- create_docs.sh
+++ create_docs.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-set -x
+#set -x
 
-python3 setup.py build
-
-unset PYTHONPATH
-BUILD_DIR=$(find $PWD/build -mindepth 1 -name "lib.*" -type d)
-export PYTHONPATH=$BUILD_DIR:$PYTHONPATH
-pdoc --html -c show_source_code=True --force --output-dir 'docs' grib2io
+# ---------------------------------------------------------------------------------------- 
+# Uses pdoc (https://github.com/mitmproxy/pdoc)
+# ---------------------------------------------------------------------------------------- 
+pdoc -o 'docs' grib2io 
