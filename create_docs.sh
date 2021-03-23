12d55a6df727352df679492883bba7cd7ffeec00
Eric Engle <eengl@users.noreply.github.com>
2021-03-23 08:06:05 -0400

    Update create_docs.sh
    Add machine hardware string to find -name string so docs can be build on Intel (x86_64) or Arm (arm64).

--- create_docs.sh
+++ create_docs.sh
@@ -4,5 +4,6 @@
 # ---------------------------------------------------------------------------------------- 
 # Uses pdoc (https://github.com/mitmproxy/pdoc)
 # ---------------------------------------------------------------------------------------- 
-libdir=$(find $PWD/build -name "lib*" -type d)
+
+libdir=$(find $PWD/build -name "lib*$(uname -m)*" -type d)
 pdoc -o 'docs' $libdir/grib2io/
