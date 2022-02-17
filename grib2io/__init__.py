cccfcba575d46be8f70f40c234abd9991a53da26
Eric Engle <eric.engle@mac.com>
2022-02-17 11:42:22 -0500

    Update __init__.py
    Add version string to config header text.

--- grib2io/__init__.py
+++ grib2io/__init__.py
@@ -14,7 +14,7 @@ def show_config():
     have_png = True if 'png' in __config__.libraries else False
     jpeglib = 'OpenJPEG' if 'openjp2' in __config__.libraries else ('Jasper' if 'jasper' in __config__.libraries else None)
     pnglib = 'libpng' if 'png' in __config__.libraries else None
-    print("grib2io Configuration:\n")
+    print("grib2io version %s Configuration:\n"%(__version__))
     print("\tg2c library version:".expandtabs(4),__config__.g2clib_version)
     print("\tJPEG compression support:".expandtabs(4),have_jpeg)
     if have_jpeg: print("\t\tLibrary:".expandtabs(4),jpeglib)
