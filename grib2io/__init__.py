8ac790ebfc02296dfe438e7b6a371b1047353c1e
Eric Engle <eric.engle@mac.com>
2022-02-18 10:46:48 -0500

    Update __init__.py
    grab g2clib version string from g2clib module.

--- grib2io/__init__.py
+++ grib2io/__init__.py
@@ -10,12 +10,13 @@ def show_config():
     """
     Print grib2io build configuration information.
     """
+    from g2clib import __version__ as g2clib_version
     have_jpeg = True if 'jasper' in __config__.libraries or 'openjp2' in __config__.libraries else False
     have_png = True if 'png' in __config__.libraries else False
     jpeglib = 'OpenJPEG' if 'openjp2' in __config__.libraries else ('Jasper' if 'jasper' in __config__.libraries else None)
     pnglib = 'libpng' if 'png' in __config__.libraries else None
     print("grib2io version %s Configuration:\n"%(__version__))
-    print("\tg2c library version:".expandtabs(4),__config__.g2clib_version)
+    print("\tg2c library version:".expandtabs(4),g2clib_version)
     print("\tJPEG compression support:".expandtabs(4),have_jpeg)
     if have_jpeg: print("\t\tLibrary:".expandtabs(4),jpeglib)
     print("\tPNG compression support:".expandtabs(4),have_png)
