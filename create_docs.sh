d9623fed0189345964309c4e411d3bda7dcce1b1
Eric Engle <eengl@users.noreply.github.com>
2022-06-10 14:49:41 -0400

    Update docs
    [skip ci]

--- create_docs.sh
+++ create_docs.sh
@@ -2,4 +2,7 @@
 # ---------------------------------------------------------------------------------------- 
 # Uses pdoc (https://github.com/mitmproxy/pdoc)
 # ---------------------------------------------------------------------------------------- 
-pdoc -o 'docs' ./grib2io ./grib2io/tables ./grib2io/utils
+sysarch=$(uname -m)
+build_dir=$(find . -name "lib.*${sysarch}*" -type d)
+echo "Building docs from: $build_dir"
+pdoc --show-source -o 'docs' $build_dir/grib2io
