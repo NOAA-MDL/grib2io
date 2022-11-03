1d000cba883b81f7a8b03f4a5a4c862c83b0ef1d
EricEngle-NOAA <eric.engle@noaa.gov>
2022-11-03 10:57:26 -0400

    Adding new table file, section6.py.
    This only contains 1 table, '6.0'.

---
+++ grib2io/tables/section6.py
@@ -0,0 +1,7 @@
+table_6_0 = {
+'0':'A bit map applies to this product and is specified in this section.',
+'1-253':'A bit map pre-determined by the orginating/generating center applies to this product and is not specified in this section.',
+'254':'A bit map previously defined in the same GRIB2 message applies to this product.',
+'255':'A bit map does not apply to this product.',
+}
+
