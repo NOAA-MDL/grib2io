84c899360684548edd59f7ba1545bf9058bcaf8f
Eric Engle <51966428+EricEngle-NOAA@users.noreply.github.com>
2023-05-17 15:23:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -197,6 +197,11 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+from setuptools.command.bdist_egg import bdist_egg
+class customize_bdist_egg(bdist_egg):
+    def run(self):
+        pass
+cmdclass['bdist_egg'] = customize_bdist_egg
 # ----------------------------------------------------------------------------------------
 # Run setup.py
 # ----------------------------------------------------------------------------------------
