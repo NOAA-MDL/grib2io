158d2638a56c052eb608bd46e665d4fe0574c01b
Eric Engle <EricEngle-NOAA@users.noreply.github.com>
2024-01-23 10:26:15 -0500

    Remove usage of datetime.utctimestamp
    Replace with datetime.timestamp(timestamp, datetime.UTC)

--- grib2io/_grib2io.py
+++ grib2io/_grib2io.py
@@ -528,7 +528,7 @@ class Grib2Message:
                       section5: np.array = None, *args, **kwargs):
 
         if np.all(section1==0):
-            section1[5:11] = datetime.datetime.utcfromtimestamp(0).timetuple()[:6]
+            section1[5:11] = datetime.datetime.fromtimestamp(0, datetime.UTC).timetuple()[:6]
 
         bases = list()
         if section3 is None:
