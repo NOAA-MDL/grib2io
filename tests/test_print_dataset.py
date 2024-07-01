f499a05e9e6cb6386d07f295544c35dac967c145
TimothyCera-NOAA <154259143+TimothyCera-NOAA@users.noreply.github.com>
2024-07-01 13:05:23 -0400

    support printing Datasets on xarray>2024.1.0 (#153)
    * fix dtype to work with xarray > 202401
    
    * move print dataset test to new file
    
    * fixed test that wasn't working if xarray<2024.2.0
    
    * simplify print test
    
    * just need to see of repr(ds1) works, not test the results

---
+++ tests/test_print_dataset.py
@@ -0,0 +1,22 @@
+import xarray as xr
+
+
+def test_da_repr(tmp_path, request):
+    """Test writing a single DataArray to a single grib2 message."""
+    target_dir = tmp_path / "test_to_grib2"
+    target_dir.mkdir()
+
+    datadir = request.config.rootdir / "tests" / "data" / "gfs_20221107"
+
+    filters = {
+        "productDefinitionTemplateNumber": 0,
+        "typeOfFirstFixedSurface": 1,
+        "shortName": "TMP",
+    }
+    ds1 = xr.open_dataset(
+        datadir / "gfs.t00z.pgrb2.1p00.f012_subset",
+        engine="grib2io",
+        filters=filters,
+    )
+
+    _ = repr(ds1)
