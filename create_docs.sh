dade6ae9555bdb6075cb3dee13136353145b35a0
Eric Engle <EricEngle-NOAA@users.noreply.github.com>
2024-08-09 11:40:54 -0400

    Update docs.
    [skip-ci]

--- create_docs.sh
+++ create_docs.sh
@@ -4,7 +4,4 @@
 #
 # pip install pdoc
 # ---------------------------------------------------------------------------------------- 
-sysarch=$(uname -m)
-build_dir=$(find . -name "lib.*${sysarch}*" -type d)
-echo "Building docs from: $build_dir"
-pdoc --docformat numpy -o 'docs' $build_dir/grib2io
+pdoc --docformat numpy -o 'docs' ./src/grib2io
