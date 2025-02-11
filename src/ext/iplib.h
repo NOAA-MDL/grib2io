01012c294e46a51a945c4983c4829e46665cb38a
Eric Engle <EricEngle-NOAA@users.noreply.github.com>
2025-02-11 13:21:30 -0500

    Adding iplib.h and iplib.pyx
    iplib.h - C header that defines ipolates_grib2 and ipolatev_grib2
    functions that map to the Fortran subroutines.
    
    iplib.pyx - Cython source file that contains the wrapper functions
    to the C-based functions.

---
+++ src/ext/iplib.h
@@ -0,0 +1,27 @@
+#ifndef IPLIB_H
+#define IPLIB_H
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ipolates_grib2(int* ip, int* ipopt, int* igdtnumi, int* igdtmpli, int* igdtleni, 
+                    int* igdtnumo, int* igdtmplo, int* igdtleno, 
+                    int* mi, int* mo, int* km, int* ibi, bool* li, float* gi, 
+                    int* no, float* rlat, float* rlon, int* ibo, bool* lo, float* go, int* iret);
+
+
+void ipolatev_grib2(int* ip, int* ipopt, int* igdtnumi, int* igdtmpli, int* igdtleni,
+                    int* igdtnumo, int* igdtmplo, int* igdtleno,
+                    int* mi, int* mo, int* km, int* ibi, bool* li, float* ui, float* vi,
+                    int* no, float* rlat, float* rlon, float* crot, float* srot, int* ibo, bool* lo,
+                    float* uo, float* vo, int* iret);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
