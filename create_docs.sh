20543865c3f1efd5b6efb20705675aca2c0ca83c
Eric Engle <EricEngle-NOAA@users.noreply.github.com>
2025-07-26 13:16:31 -0400

    Update docs and version bump.
    [skip ci]

--- create_docs.sh
+++ create_docs.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 # ---------------------------------------------------------------------------------------- 
-# Uses real pdoc (https://github.com/mitmproxy/pdoc)
+# Uses real pdoc (https://github.com/mitmproxy/pdoc), not pdoc3
 #
 # pip install pdoc
 # ---------------------------------------------------------------------------------------- 
-BUILD_LIB=$(find . -name "lib*" -type d)
-pdoc --docformat numpy -o 'docs' $BUILD_LIB/grib2io 
+
+# Build in place
+python setup.py build_ext --inplace
+
+# Build docs
+PYTHONPATH=$PWD/src pdoc --docformat numpy -o docs grib2io 
+
+# Clean up
+find src/grib2io -name "*.so" -delete
+find src/grib2io -name "*.pyd" -delete
+find src/grib2io -name "*.pyc" -delete
+find src/grib2io -name "__pycache__" -type d -exec rm -r {} +
